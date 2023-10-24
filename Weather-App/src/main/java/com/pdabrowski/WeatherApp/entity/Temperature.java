72848c797a28763e2e661cead8b5f574b96f2051
pawiik <paweld4642@gmail.com>
2023-10-24 16:43:18 +0200

    completed Temperature entity class

--- Weather-App/src/main/java/com/pdabrowski/WeatherApp/entity/Temperature.java
+++ Weather-App/src/main/java/com/pdabrowski/WeatherApp/entity/Temperature.java
@@ -1,4 +1,40 @@
 package com.pdabrowski.WeatherApp.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="temperature")
 public class Temperature {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "measurement_id")
+    private int measurement_id;
+
+    @Column(name="time")
+    private int time;
+
+    @Column(name="temperature")
+    private double temperature;
+
+    @Column(name="measurement_station_id")
+    private int measurementStationId;
+
+    public Temperature(){}
+
+    public Temperature(int time, double temperature, int measurementStationId) {
+        this.time = time;
+        this.temperature = temperature;
+        this.measurementStationId = measurementStationId;
+    }
+
+    @Override
+    public String toString() {
+        return "Temperature{" +
+                "measurement_id=" + measurement_id +
+                ", time=" + time +
+                ", temperature=" + temperature +
+                ", measurementStationId=" + measurementStationId +
+                '}';
+    }
 }
