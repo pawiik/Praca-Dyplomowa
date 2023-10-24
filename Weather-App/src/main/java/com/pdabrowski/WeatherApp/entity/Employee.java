e364f443ee8867f02cea1705bffabbdcf1ef0635
pawiik <paweld4642@gmail.com>
2023-10-24 19:32:43 +0200

    completed implementation of Employee entity class

--- Weather-App/src/main/java/com/pdabrowski/WeatherApp/entity/Employee.java
+++ Weather-App/src/main/java/com/pdabrowski/WeatherApp/entity/Employee.java
@@ -1,4 +1,99 @@
 package com.pdabrowski.WeatherApp.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "employee_id")
+    private int employeeId;
+
+    @Column(name = "measurement_station_id")
+    private int measurementStationId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "phone_number")
+    private int phoneNumber;
+
+    @Column(name = "address")
+    private String address;
+
+    public Employee(){
+    }
+
+    public Employee(int measurementStationId, String name, String lastName, int phoneNumber, String address) {
+        this.measurementStationId = measurementStationId;
+        this.name = name;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public int getMeasurementStationId() {
+        return measurementStationId;
+    }
+
+    public void setMeasurementStationId(int measurementStationId) {
+        this.measurementStationId = measurementStationId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", measurementStationId=" + measurementStationId +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
