bfa3b969728121b787f0385edb9fb3e11426a6b7
pawiik <paweld4642@gmail.com>
2023-10-24 19:50:49 +0200

    completed implementation of User entity class

--- Weather-App/src/main/java/com/pdabrowski/WeatherApp/entity/User.java
+++ Weather-App/src/main/java/com/pdabrowski/WeatherApp/entity/User.java
@@ -1,4 +1,72 @@
 package com.pdabrowski.WeatherApp.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "city_id")
+    private int cityId;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "region_id")
+    private int regionId;
+
+    public User(){}
+
+    public User(int cityId, String address, int regionId) {
+        this.cityId = cityId;
+        this.address = address;
+        this.regionId = regionId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getRegionId() {
+        return regionId;
+    }
+
+    public void setRegionId(int regionId) {
+        this.regionId = regionId;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", cityId=" + cityId +
+                ", address='" + address + '\'' +
+                ", regionId=" + regionId +
+                '}';
+    }
 }
