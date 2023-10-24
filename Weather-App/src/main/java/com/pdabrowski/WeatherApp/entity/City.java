504d08fc8da85e053d755248af869029b02fd2fd
pawiik <paweld4642@gmail.com>
2023-10-24 18:08:10 +0200

    completed implementation of City entity class

--- Weather-App/src/main/java/com/pdabrowski/WeatherApp/entity/City.java
+++ Weather-App/src/main/java/com/pdabrowski/WeatherApp/entity/City.java
@@ -1,4 +1,59 @@
 package com.pdabrowski.WeatherApp.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "city")
 public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "city_id")
+    private int cityId;
+
+    @Column(name = "city_name")
+    private String cityName;
+
+    @Column(name = "region_id")
+    private int regionId;
+
+    public City(){}
+
+    public City(String cityName, int regionId) {
+        this.cityName = cityName;
+        this.regionId = regionId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getRegionId() {
+        return regionId;
+    }
+
+    public void setRegionId(int regionId) {
+        this.regionId = regionId;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "cityId=" + cityId +
+                ", cityName='" + cityName + '\'' +
+                ", regionId=" + regionId +
+                '}';
+    }
 }
