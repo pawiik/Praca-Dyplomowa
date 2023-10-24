e7a8c2bafdd8e948e7a33efde2775e4f2bb0da4f
pawiik <paweld4642@gmail.com>
2023-10-24 12:13:56 +0200

    added Region entity

---
+++ Weather-App/src/main/java/com/pdabrowski/WeatherApp/entity/Region.java
@@ -0,0 +1,42 @@
+package com.pdabrowski.WeatherApp.entity;
+
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Entity
+@Table(name="region")
+public class Region {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="region_id")
+    private int region_id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Region(){
+
+    }
+
+    public Region(int region_id, String name) {
+        this.region_id = region_id;
+        this.name = name;
+    }
+
+    public int getRegion_id() {
+        return region_id;
+    }
+
+    public void setRegion_id(int region_id) {
+        this.region_id = region_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
