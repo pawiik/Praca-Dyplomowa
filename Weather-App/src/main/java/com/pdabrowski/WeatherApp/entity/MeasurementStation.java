e37db4b0c2e28e0db138ca275d453c1d96a75989
pawiik <paweld4642@gmail.com>
2023-10-24 19:41:50 +0200

    completed implementation of MeasurementStation entity class

--- Weather-App/src/main/java/com/pdabrowski/WeatherApp/entity/MeasurementStation.java
+++ Weather-App/src/main/java/com/pdabrowski/WeatherApp/entity/MeasurementStation.java
@@ -1,4 +1,72 @@
 package com.pdabrowski.WeatherApp.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "measurement_station")
 public class MeasurementStation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "station_id")
+    private int stationId;
+
+    @Column(name = "city_id")
+    private int cityId;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "region_id")
+    private int regionId;
+
+    public MeasurementStation(){
+    }
+
+    public MeasurementStation(int cityId, String address, int regionId) {
+        this.cityId = cityId;
+        this.address = address;
+        this.regionId = regionId;
+    }
+
+    public int getStationId() {
+        return stationId;
+    }
+
+    public void setStationId(int stationId) {
+        this.stationId = stationId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getRegionId() {
+        return regionId;
+    }
+
+    public void setRegionId(int regionId) {
+        this.regionId = regionId;
+    }
+
+    @Override
+    public String toString() {
+        return "MeasurementStation{" +
+                "stationId=" + stationId +
+                ", cityId=" + cityId +
+                ", address='" + address + '\'' +
+                ", regionId=" + regionId +
+                '}';
+    }
 }
