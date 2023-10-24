14940335d12b99b0d39c3b8c51f0670b45f4b637
pawiik <paweld4642@gmail.com>
2023-10-24 17:43:33 +0200

    completed implementation of Alert entity class

--- Weather-App/src/main/java/com/pdabrowski/WeatherApp/entity/Alert.java
+++ Weather-App/src/main/java/com/pdabrowski/WeatherApp/entity/Alert.java
@@ -1,4 +1,50 @@
 package com.pdabrowski.WeatherApp.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "alert")
 public class Alert {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "alert_id")
+    private int alertId;
+
+    @Column(name = "start_time")
+    private int startTime;
+
+    @Column(name = "end_time")
+    private int endTime;
+
+    @Column(name = "region_id")
+    private int regionId;
+
+    @Column(name = "message")
+    private String message;
+
+    @Column(name = "alert_type")
+    private int aletrType;
+
+    public Alert(){}
+
+    public Alert(int startTime, int endTime, int regionId, String message, int aletrType) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.regionId = regionId;
+        this.message = message;
+        this.aletrType = aletrType;
+    }
+
+    @Override
+    public String toString() {
+        return "Alert{" +
+                "alertId=" + alertId +
+                ", startTime=" + startTime +
+                ", endTime=" + endTime +
+                ", regionId=" + regionId +
+                ", message='" + message + '\'' +
+                ", aletrType=" + aletrType +
+                '}';
+    }
 }
