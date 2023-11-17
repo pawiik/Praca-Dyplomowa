75a8c3fa9b048fd28455ebfc837da5be2b173bec
pawiik <paweld4642@gmail.com>
2023-11-17 13:56:50 +0100

    added user service

--- src/app/weather/services/user-service.ts
+++ src/app/weather/services/user-service.ts
@@ -1,15 +1,16 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import {City} from "../../shared/model/City";
 import {User} from "../../shared/model/User";
 import {UserInformationComponent} from "../user-information/user-information.component";
+import {AuthService} from "./auth";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService{
   private apiUrl = 'http://localhost:8080/user'
-  constructor(private httpClient: HttpClient){
+  constructor(private httpClient: HttpClient, private authService: AuthService){
   }
 
   getUserInformation(userId: string){
@@ -17,6 +18,16 @@ export class UserService{
     console.log(address)
     return this.httpClient.get<User>(address);
 
+  }
+
+  updateUserInformation(body: {}){
+    let jwtToken = this.authService.authData.jwtToken
+    let options = {
+      headers: new HttpHeaders({
+        "Authorization": jwtToken,
+      })
+    }
+    return this.httpClient.put(this.apiUrl, body, options)
   }
 
 }
