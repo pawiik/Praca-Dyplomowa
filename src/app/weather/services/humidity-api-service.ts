a49bf56abe7ff413db4ae3ecaf9e647bef9e60cf
pawiik <paweld4642@gmail.com>
2023-11-21 19:23:23 +0100

    added humidity api service

---
+++ src/app/weather/services/humidity-api-service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {AuthService} from "./auth";
+import {Observable} from "rxjs";
+import {Humidity} from "../../shared/model/Humidity";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HumidityApiService {
+
+  private apiUrl: string = 'http://localhost:8080/humidity'
+
+  constructor(private httpClient: HttpClient, private authService: AuthService) {
+  }
+
+  public getAllHumidity(): Observable<Humidity[]> {
+    let jwtToken: string = this.authService.authData.jwtToken
+    let options: {} = {
+      headers: new HttpHeaders({
+        "Authorization": jwtToken
+      })
+    }
+    return this.httpClient.get<Humidity[]>(this.apiUrl + "/falls", options);
+  }
+
+  public addNewFall(body: {}): Observable<Humidity> {
+    let jwtToken: string = this.authService.authData.jwtToken
+    let options: {} = {
+      headers: new HttpHeaders({
+        "Authorization": jwtToken
+      })
+    }
+    return this.httpClient.post<Humidity>(this.apiUrl + "/", options, body)
+  }
+}
