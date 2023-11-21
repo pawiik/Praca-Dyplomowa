a277322b0e5ffa590b813160d3af7e28d6246310
pawiik <paweld4642@gmail.com>
2023-11-21 21:51:06 +0100

    added employee api service

---
+++ src/app/weather/services/employee-api-service.ts
@@ -0,0 +1,47 @@
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {AuthService} from "./auth-service";
+import {Observable} from "rxjs";
+import {Fall} from "../../shared/model/Fall";
+import {Employee} from "../../shared/model/Employee";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FallApiService {
+
+  private apiUrl: string = 'http://localhost:8080/employee'
+
+  constructor(private httpClient: HttpClient, private authService: AuthService) {
+  }
+
+  public getAllEmployees(): Observable<Employee[]> {
+    let jwtToken: string = this.authService.authData.jwtToken
+    let options: {} = {
+      headers: new HttpHeaders({
+        "Authorization": jwtToken
+      })
+    }
+    return this.httpClient.get<Employee[]>(this.apiUrl + "/employees", options);
+  }
+
+  public getEmployeeById(employeeId: number): Observable<Employee>{
+    let jwtToken: string = this.authService.authData.jwtToken
+    let options: {} = {
+      headers: new HttpHeaders({
+        "Authorization": jwtToken
+      })
+    }
+    return this.httpClient.get<Employee>(this.apiUrl + employeeId, options);
+  }
+
+  public addNewEmployee(body: {}): Observable<Employee> {
+    let jwtToken: string = this.authService.authData.jwtToken
+    let options: {} = {
+      headers: new HttpHeaders({
+        "Authorization": jwtToken
+      })
+    }
+    return this.httpClient.post<Employee>(this.apiUrl + "/", options, body)
+  }
+}
