ab38b454b2b2f5aea344a5c51b076be9804560a9
pawiik <paweld4642@gmail.com>
2023-11-21 16:29:14 +0100

    added alert api service

---
+++ src/app/weather/services/alert-service.ts
@@ -0,0 +1,37 @@
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {AuthService} from "./auth";
+import {Observable} from "rxjs";
+import {Alert} from "../../shared/model/Alert";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+  private apiUrl = 'http://localhost:8080/alert'
+
+  constructor(private httpClient: HttpClient, private authService: AuthService) {
+  }
+
+  public getAllAlerts(): Observable<Alert[]>{
+    let jwtToken: string = this.authService.authData.jwtToken
+    let options: HttpHeaders = new HttpHeaders({
+      "Authorization": jwtToken
+    })
+    let body = {headers: options}
+    return this.httpClient.get<Alert[]>(this.apiUrl + "/", body)
+  }
+
+  public addNewAlert(body: {}): Observable<Alert>{
+    let jwtToken: string = this.authService.authData.jwtToken
+    let options: {} = {
+      headers: new HttpHeaders({
+        "Authorization": jwtToken,
+      })
+    }
+
+    return this.httpClient.post<Alert>(this.apiUrl + "/", body, options)
+  }
+
+
+}
