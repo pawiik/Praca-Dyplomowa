2c6d3a00684b45a7f0772a8562c356841eb25673
pawiik <paweld4642@gmail.com>
2024-01-16 17:42:47 +0100

    improved register form

--- src/app/weather/register/register.component.ts
+++ src/app/weather/register/register.component.ts
@@ -4,6 +4,7 @@ import {Router} from "@angular/router";
 import {AuthService} from "../services/auth-service";
 import {City} from "../../shared/model/City";
 import {CityApiService} from "../services/city-api-service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 
 
@@ -14,15 +15,30 @@ import {CityApiService} from "../services/city-api-service";
 })
 export class RegisterComponent {
 
-  formData = {cityId:'', name: '', lastName: '', emailAddress: '', password: '', confirmPassword: '' };
+  formData = {cityId: '', name: '', lastName: '', emailAddress: '', password: '', confirmPassword: ''};
   cities: City [] = []
+  registrationForm: FormGroup;
 
 
   constructor(private http: HttpClient, private router: Router, private authService: AuthService, private cityService: CityApiService) {
     this.loadCities()
+    this.registrationForm = new FormGroup({
+      cityId: new FormControl('', Validators.required),
+      name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+      emailAddress: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#?$\\]]).{8,}$')
+      ]),
+      confirmPassword: new FormControl('', Validators.required),
+    });
+
   }
 
-  loadCities(){
+
+
+  loadCities() {
     this.cityService.getAllCities().subscribe(response => this.cities = response)
   }
 
@@ -36,6 +52,10 @@ export class RegisterComponent {
 
     this.authService.registerUser(body)
     this.router.navigate(['/login'])
+  }
+
+  checkIfEmailExists(email: string): boolean {
+    return false;
   }
 }
 
